7327701698d33b0716d824dcbe2240d093740c1f
keighbee <kb@huggingface.co>
2024-12-11 13:28:01 -0800

    removed accidentally commited redundant files

--- coreml_export_even_older.py
+++
@@ -1,6 +0,0 @@
-import demucs.api
-filename = "test2.mp3"
-separator = demucs.api.Separator()
-origin, separated = separator.separate_audio_file(filename)
-for stem, audio_data in separated.items():
-    demucs.api.save_audio(audio_data, f"separated/coreml_even_older/{stem}_{filename}", samplerate=separator.samplerate)
